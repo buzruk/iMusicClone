be11e8697dd3d5316baaffeda13f697f0f680c52
Buzurgmexr Sultonaliyev <sultonaliyevbuzurgmexr@gmial.com>
2023-08-27 09:34:24 +0500

    - 🥳 remove main storyboard
    - 🥳 add MainTabBarController rootViewController for window

---
+++ iMusicClone/TabBar/MainTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabBarController.swift
+//  iMusicClone
+//
+//  Created by Buzurgmexr Sultonaliyev on 27/08/23.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
